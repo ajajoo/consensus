ef6906ab7aaf4a4975a1624a1549a9cf55fd1c7d
ajajoo <akshay.vns@gmail.com>
2016-03-26 12:10:41 -0400

    lost update of leader.py on change is coding style added

--- leader.py
+++ leader.py
@@ -16,36 +16,35 @@ def raiseError(message="", exit = False, exitCode = 1):
     print message
     if exit:
         sys.exit(exitCode)
+        
+def extractArgFor(thisOption): 
+    for x in range(1,len(sys.argv),2):
+        if(sys.argv[x])==thisOption:
+            return sys.argv[x+1]
+    return ""
 
 def checkArgValidity():
     if len(sys.argv)<7:
         raiseError(message="Too few arguments.", exit = True)
     if len(sys.argv)>7:
         raiseError(message="Too many arguments.", exit = True)
-    port = 0
-    for x in range(1,len(sys.argv),2):
-        if(sys.argv[x])=="-p":  #ports
-            port = sys.argv[x+1]
+    port = extractArgFor("-p")
     if (port.isdigit()==False):
         raiseError(message="Port should be integer.", exit = True)
     elif 1023<int(port)<65536:
         pass
     else:
         raiseError(message="Port is out of range.", exit = True)
-    hostFile =""
-    for x in range(1,len(sys.argv),2):
-        if(sys.argv[x])=="-h":   #hostFile
-            hostFile= sys.argv[x+1]
+    hostFile = extractArgFor("-h")
+    if hostFile == "":
+        raiseError(message="Hostfile is non existant", exit = True)
     numHost = sum(1 for line in open(hostFile))
     if numHost<1:
         raiseError(message="Hostfile is empty", exit = True)
-    maxCrashes = -1
-    for x in range(1,len(sys.argv),2):
-        if(sys.argv[x])=="-f":   #maxCrashes
-            maxCrashes = sys.argv[x+1]
+    maxCrashes = extractArgFor("-f")
     if (maxCrashes.isdigit()==False):
         raiseError(message="Number of max crash should be integer.", exit = True)
-    elif 0<int(maxCrashes)<numHost-1:
+    elif 0<int(maxCrashes)<numHost-2:
         pass
     else:
         raiseError(message="Max crash is out of range.", exit = True)
@@ -93,7 +92,6 @@ def setUpMessagePrinter():
                         print data
                         insock.close()
                         inputsockets.remove(insock)
-    print "Port ",port
     return (socket.gethostbyname(socket.gethostname()),port)
 
 class Host(object):
