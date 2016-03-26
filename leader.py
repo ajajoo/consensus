c47339f4c49222a5ce98194e72481e268d0ead85
ajajoo <akshay.vns@gmail.com>
2016-03-26 12:37:29 -0400

    pramiko and authenticate related part removed from leader.py

--- leader.py
+++ leader.py
@@ -3,7 +3,6 @@ import os
 import re
 import numpy as np
 import subprocess
-#import paramiko
 import getpass
 import socket
 import SocketServer
@@ -11,7 +10,6 @@ from subprocess import Popen, PIPE
 import time
 from select import select
 
-globalmsgarr=[]
 def raiseError(message="", exit = False, exitCode = 1):
     print message
     if exit:
@@ -51,11 +49,6 @@ def checkArgValidity():
 
     return int(port), hostFile, int(maxCrashes)
 
-def getLoginInfo():
-    uname = raw_input("Please enter user name to be used to login on other: ")
-    pwd = getpass.getpass()
-    return uname, pwd
-
 def editFile(filePath,lineNums,newLines):
     f =  open(filePath)
     temp = f.readlines()
@@ -72,8 +65,7 @@ def setUpMessagePrinter():
     s.bind((socket.gethostname(),0))
     port = s.getsockname()[1]
     s.listen(50)
-    #ip, port = server.server_address # Fork a new process with the server -- that process will then fork one more process for each request
-    timeout = 1000
+    timeout = 2000
     inputsockets = [s]
     t = os.fork()
     if  t==0:
@@ -95,18 +87,14 @@ def setUpMessagePrinter():
     return (socket.gethostbyname(socket.gethostname()),port)
 
 class Host(object):
-    def __init__(self, hid, hname, pathToScript):# uname, pwd):
+    def __init__(self, hid, hname, pathToScript):
         self.id = hid;
         self.name = hname;
-        #return self
-    #def execute(self, pathToScript, uname, pwd):
         command = 'python '+pathToScript
         ssh = subprocess.Popen(["ssh", "%s" %self.name, command],shell=False,stdout=None, stderr=None, stdin=None)
-        #ssh = paramiko.SSHClient() #ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())#ssh.connect(hostname = self.name, username=uname, password=pwd)#stdin, stdout, stderr = ssh.exec_command(command)#ssh.close()
 
 if __name__ == '__main__':
     lPort, hostFile, maxCrashes = checkArgValidity()
-    #uname, pwd = getLoginInfo()
     processScript = os.getcwd()+'/process.py'
     mHost, mPort = setUpMessagePrinter()
     hosts = {}
@@ -116,4 +104,4 @@ if __name__ == '__main__':
             hosts[int(x[0])] = x[1]
     editFile(processScript,[0,1,2,3,4,5],['lPort = '+str(lPort)+'\n','mPort = '+str(mPort)+'\n','mHost = \"'+str(mHost)+'\"\n','otherHosts = '+str(hosts)+'\n','waitTill = '+str(time.time()+len(hosts))+'\n','maxCrashes = '+str(maxCrashes)+'\n'])
     for hid in hosts:
-        Host(hid, hosts[hid], processScript)# uname, pwd)
+        Host(hid, hosts[hid], processScript)
